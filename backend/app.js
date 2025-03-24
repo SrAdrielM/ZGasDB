40933437c19798d23682593aa18ac01bb9e5282b
SrAdrielM <20230542@ricaldone.edu.sv>
2025-03-24 15:40:57 -0600

    pupu

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 //importo todo lo de la libreria express
 import express from "express";
+import cookieParser from "cookie-parser";
 
 import productsRoutes from "./src/routes/products.js"
 import clientsRouter from "./src/routes/clients.js"
@@ -7,12 +8,14 @@ import employeesRouter from "./src/routes/employees.js"
 import branchesRouter from "./src/routes/branches.js"
 import reviewsRouter from "./src/routes/reviews.js";
 import evaluationsRouter from "./src/routes/evaluations.js"
+import registerEmployeesRouter from "./src/routes/registerEmployees.js";
 
 
 //creo una constante que es igual a la libreria que acabo de importar
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/products", productsRoutes);
 app.use("/api/clients", clientsRouter);
@@ -20,6 +23,7 @@ app.use("/api/employees", employeesRouter);
 app.use("/api/branches", branchesRouter);
 app.use("/api/reviews", reviewsRouter)
 app.use("/api/evaluations", evaluationsRouter);
+app.use("/api/registerEmployees", registerEmployeesRouter);
 
 
 //exporto esta constante para usar express en todo lados
---
+++ backend/src/controllers/registerEmployeesCtrl.js
@@ -0,0 +1,43 @@
+import bcryptjs from "bcryptjs";
+import jsonwebtoken from "jsonwebtoken"
+
+import {config} from "../config.js"
+import employeesMdl from "../models/employeesMdl.js";
+
+const registerEmployeesController = {}
+
+registerEmployeesController.register = async (req, res) =>{
+    const { name, lastName, birthday, email, address, hireDate, password, telephone, dui, isssNumber, isVerified  } = req.body
+
+    try {
+        //verificar si el empleado existe
+        const existEmployee = await employeesMdl.findOne({email})
+        if (existEmployee) {
+            return res.json({message: "empleado ya existente"})
+        }
+
+        const passwordHash = await bcryptjs.hash(password, 10)
+
+        const newEmployee = new employeesMdl({ name, lastName, birthday, email, address, hireDate, password: passwordHash, telephone, dui, isssNumber, isVerified })
+
+        await newEmployee.save();
+
+        jsonwebtoken.sign(
+            //1- que voy a guardar
+            {id: newEmployee._id},
+            //2- secreto
+            config.JWT.secret,
+            //3- cuando expira
+            {expiresIn: config.JWT.expiresIn},
+            //4- funcion flecha
+            (error, token) => {
+                if(error)console.log(error)
+                res.cookie("authToken", token)
+            }
+        )
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export default registerEmployeesController;
